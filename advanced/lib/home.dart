3554b9e08e068601da4737be0c6f80db9644d75d
Dentro2000 <szk@MacBook-Pro-SZK.local>
2019-11-30 14:55:51 +0100

    add some data, show 10 cards

--- advanced/lib/home.dart
+++ advanced/lib/home.dart
@@ -25,8 +25,8 @@ class HomePage extends StatelessWidget {
       body: GridView.count(
         crossAxisCount: 2,
         padding: EdgeInsets.all(2),
-        childAspectRatio: 8 / 9,
-        children: <Widget>[Card()],
+        childAspectRatio: 8.0 / 9.0,
+        children: _buildGridCards(10),
       ),
       appBar: AppBar(
         title: Text("SHRINE"),
@@ -62,5 +62,34 @@ class HomePage extends StatelessWidget {
       ),
       resizeToAvoidBottomInset: false,
     );
+  }
+
+  List<Card> _buildGridCards(int count) {
+    List<Card> cards = List.generate(
+        count,
+        (int index) => Card(
+              clipBehavior: Clip.antiAlias,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  AspectRatio(
+                    aspectRatio: 18.0 / 11.0,
+                    child: Image.asset('assets/diamond.png'),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('Title'),
+                        SizedBox(height: 8.0),
+                        Text('Secondary Text')
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ));
+    return cards;
   }
 }
